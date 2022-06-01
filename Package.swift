cca0e2e002efa5572e2043fbd3e4dca5966b5cf7
Juan Ledesma <juledesma@paypal.com>
2022-06-01 13:05:34 -0400

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "ContactsKit",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "ContactsKit",
+            targets: ["ContactsKit"]
+        ),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "ContactsKit",
+            dependencies: [],
+            resources: []
+        ),
+        .testTarget(
+            name: "ContactsKitTests",
+            dependencies: [
+                "ContactsKit",
+            ]
+        ),
+    ]
+)
